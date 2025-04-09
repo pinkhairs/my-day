e40e40d22e065a9210a1c289bc43cc9dca9ae801
Diana <me@diana.nu>
2025-04-08 21:35:23 -0700

    ok FIXED css not showing up (vite config error)

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/',
   plugins: [
     react(),
+    tailwindcss(),
     {
       name: 'log-build-paths',
       buildStart() {
